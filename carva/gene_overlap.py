8e1f21e19687abed001b9cdac994ed508581493e
root <root@rack-elkon-03.cs.tau.ac.il>
2025-10-30 10:25:59 +0200

    made gene overlap simpler to use

--- carva/gene_overlap.py
+++ carva/gene_overlap.py
@@ -15,17 +15,20 @@ if __name__ == '__main__':
     parser.add_argument('--min_genes', type=int, default=3, required=False, help='Minimum number of genes')
     parser.add_argument('--background_N', type=int, default=20000, required=False, help='Background gene set size')
     parser.add_argument('--test_name', type=str, help='Name to give the test')
+    parser.add_argument('--verbose', action='store_true', help='Whether tto print the genes in the overlap (default: False)')
     args = parser.parse_args()
 
     outfile=os.path.join(args.outdir, f'{args.raretrait}_{args.commontrait}.R_C_overlap.txt')
-    rare_genes = load_gene_profile(os.path.join(args.datadir, args.raretrait + '_RV.txt'), args.rare_th, args.min_genes)
-    common_genes = load_gene_profile(os.path.join(args.datadir, args.commontrait + '_CV.txt'), args.common_th, args.min_genes)
+    rare_genes = load_gene_profile(os.path.join(args.datadir, args.raretrait + '_rv.txt'), args.rare_th, args.min_genes)
+    common_genes = load_gene_profile(os.path.join(args.datadir, args.commontrait + '_cv.txt'), args.common_th, args.min_genes)
     if (rare_genes is not None) and (common_genes is not None):
         n_rare = len(rare_genes)
         n_common = len(common_genes)
         intersect = set(rare_genes).intersection(common_genes)
         n_intersect = len(intersect)
         p = hypergeom.sf(k=n_intersect, N=n_rare, n=n_common, M=args.background_N)
+        if args.verbose:
+            print(intersect)
         with open(outfile, 'w') as f:
             f.write('\t'.join([f'{args.raretrait}_{args.commontrait}', args.test_name, str(n_common), str(n_rare), str(n_intersect), str(args.background_N), str(p)])+'\n')
         
